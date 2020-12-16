9e456f582212b89ffa00eb47e51542e00f2989ea
CJG <chjugomez@utp.edu.co>
2020-12-16 10:41:34 -0500

    Init implementation professor dto

---
+++ src/app/dto/professorDto.ts
@@ -0,0 +1,26 @@
+import { OnInit, Directive } from '@angular/core'
+
+@Directive()
+export class ProfessorDto implements OnInit {
+
+    public id : number;
+    public identificationCard : String;
+    public name : String;
+    public lastname : String;
+    public email : String;
+    public username : String;
+    public password : String;
+
+    constructor () {
+        this.id = 1;
+        this.identificationCard = new String ("");
+        this.name = new String("");
+        this.lastname = new String("");
+        this.email = new String("");
+        this.username = new String("");
+        this.password = new String("");
+    }
+
+    ngOnInit() {
+    }
+}
