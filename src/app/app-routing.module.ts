e03c7a19bc952d9a37eaf734275ef79da3ff502d
CJG <chjugomez@utp.edu.co>
2021-01-18 14:21:07 -0500

    create-professor route added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreateProfessorComponent } from './create-professor/create-professor.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'create-professor', component: CreateProfessorComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
