6ca570004d6cd631f62a133de7c498b04162da79
CJG <chjugomez@utp.edu.co>
2021-01-18 10:17:43 -0500

    ExamDto class

---
+++ src/app/dto/ExamDto.ts
@@ -0,0 +1,25 @@
+import { Directive } from '@angular/core';
+import { ProfessorDto } from './ProfessorDto';
+
+@Directive()
+export class ExamDto {
+
+    public id : Number;
+    public professor : ProfessorDto;
+    public description : String;
+    public link : String;
+    public maxGrade : Number;
+    public name : String;
+    public durability : Number;
+
+    constructor() {
+        this.id = 0;
+        this.professor = new ProfessorDto;
+        this.description = new String;
+        this.link = new String;
+        this.maxGrade = 0.0;
+        this.name = new String;
+        this.durability = 0;
+    }
+
+}
