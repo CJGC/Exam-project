ce3b70d35f79198b801735a05d4b55cb62560780
CJG <chjugomez@utp.edu.co>
2021-01-18 10:22:08 -0500

    AnswerOptionDto Class

---
+++ src/app/dto/AnswerOptionDto.ts
@@ -0,0 +1,22 @@
+import { QuestionDto } from './abstractDto/QuestionDto';
+import { SelectedResponseDto } from './SelectedResponseDto';
+
+export class AnswerOptionDto  {
+    public id : number;
+    public index : String;
+    public description : String;
+    public correctAnswer : Boolean;
+    public weight : number;
+    public selectedResponses : Array<SelectedResponseDto>;
+    public question : QuestionDto | undefined;
+
+    constructor() {
+        this.id = 0;
+        this.index = new String;
+        this.description = new String;
+        this.correctAnswer = new Boolean;
+        this.weight = 0.0;
+        this.selectedResponses = new Array<SelectedResponseDto>();
+    }
+
+}
