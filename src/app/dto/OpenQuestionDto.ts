08f56e622ab128a38ceb5665b8c5c6192d8563dc
CJG <chjugomez@utp.edu.co>
2021-01-18 10:46:01 -0500

    OpenQuestionDto class

---
+++ src/app/dto/OpenQuestionDto.ts
@@ -0,0 +1,10 @@
+import { QuestionDto } from './abstractDto/QuestionDto';
+import { AnswerOptionDto } from './AnswerOptionDto';
+
+export class OpenQuestionDto extends QuestionDto {
+    
+    constructor() {
+        super();
+    }
+
+}
