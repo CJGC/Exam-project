477ea26aefdf34fdbf993dd8bcf3acbcde71c2f2
CJG <chjugomez@utp.edu.co>
2021-01-18 10:07:43 -0500

    QuestionDto abstract class

---
+++ src/app/dto/abstractDto/QuestionDto.ts
@@ -0,0 +1,20 @@
+import {ExamDto } from '../examDto';
+
+export abstract class QuestionDto {
+
+    private id : number;
+    private description : String;
+    // private questionImage : File;
+    private weight : number;
+    private exam : ExamDto;
+    private type : String;
+
+    constructor() {
+        this.id = 0;
+        this.description = new String;
+        // this.questionImage = File();
+        this.weight = 0.0;
+        this.type = new String("");
+        this.exam = new ExamDto;
+    }
+}
