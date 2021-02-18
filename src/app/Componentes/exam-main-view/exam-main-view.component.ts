d7fdca0b9d057bef089e906d5839215d6ba96d24
CJG <chjugomez@utp.edu.co>
2021-02-18 12:53:11 -0500

    Exam main view component

--- src/app/Componentes/exam-main-view/exam-main-view.component.ts
+++ src/app/Componentes/exam-main-view/exam-main-view.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ExamDto } from 'src/app/dto/ExamDto';
 
 @Component({
   selector: 'app-exam-main-view',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExamMainViewComponent implements OnInit {
 
-  constructor() { }
+  public exams : Array<ExamDto>;
+  
+
+  constructor() { 
+    this.exams = new Array<ExamDto>();
+  }
 
   ngOnInit(): void {
+    
+  }
+
+  public createExam() : void {
+
+  }
+
+  public editExam() : void {
+
+  }
+
+  public deleteExam() : void {
+
   }
 
 }
