93f53774b61a8da8ae1d6b0a5d59b5468b8a5705
CJG <chjugomez@utp.edu.co>
2021-02-18 12:49:52 -0500

    Routing for page not found and exam main view components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CreateProfessorComponent } from './Componentes/create-professor/create-professor.component';
+import { ExamMainViewComponent } from './Componentes/exam-main-view/exam-main-view.component';
+import { PageNotFoundComponent } from './Componentes/page-not-found/page-not-found.component';
 import { ProfessorMainViewComponent } from './Componentes/professor-main-view/professor-main-view.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'create-professor', pathMatch: 'full'},
   { path: 'create-professor', component: CreateProfessorComponent},
-  { path: 'professor-main-view', component: ProfessorMainViewComponent}
+  { path: 'professor-main-view', component: ProfessorMainViewComponent, 
+    children: [
+      {path: 'exam-main-view', component: ExamMainViewComponent}
+    ]
+  },
+  { path: '', redirectTo: 'ProfessorMainViewComponent', pathMatch: 'full'},
+  { path: '**', component: PageNotFoundComponent}
 ];
-
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
