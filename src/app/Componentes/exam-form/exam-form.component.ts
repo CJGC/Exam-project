962592d51fffa588d5cde0faa38830ef31aff36c
CJG <chjugomez@utp.edu.co>
2021-02-22 15:24:46 -0500

    Exam form component

---
+++ src/app/Componentes/exam-form/exam-form.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exam-form',
+  templateUrl: './exam-form.component.html',
+  styleUrls: ['./exam-form.component.css']
+})
+export class ExamFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  public generateLink() : void {
+    let link = "exam/";
+  }
+}
