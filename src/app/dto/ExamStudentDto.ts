4bd528ec7f3ee651d537ffdf308595f29a808abe
CJG <chjugomez@utp.edu.co>
2021-03-01 08:49:01 -0500

    Removed directive annotation

--- src/app/dto/ExamStudentDto.ts
+++ src/app/dto/ExamStudentDto.ts
@@ -1,8 +1,6 @@
-import { Directive } from '@angular/core';
 import { ExamDto } from './ExamDto';
 import { StudentDto } from './StudentDto';
 
-@Directive()
 export class ExamStudentDto {
 
     public id : number;
