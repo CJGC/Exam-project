71edd924aaf190d740dc60d3810a251c9e683570
CJG <chjugomez@utp.edu.co>
2021-03-09 17:18:11 -0500

    Added methods to improve management for exams

--- src/app/Componentes/exam-form/exam-form.component.ts
+++ src/app/Componentes/exam-form/exam-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { MessageService } from 'primeng/api';
 import { ExamDto } from 'src/app/dto/ExamDto';
 import { ProfessorDto } from 'src/app/dto/ProfessorDto';
@@ -14,108 +14,128 @@ import { ProfessorService } from 'src/app/services/Professor.service';
 })
 export class ExamFormComponent implements OnInit {
 
-  @Input() creatingExam : boolean;
-  @Output() creatingExamChange : any;
+  @Input() public exams : Array<ExamDto>;
+  @Output() public examsChange : any;
 
-  @Input()  selectedAccordition : boolean;
-  @Output() selectedAccorditionChange : any;
+  public examForm : FormGroup;
+  public exam : ExamDto;
 
-  @Input() form : FormGroup;
-  @Output() formChange : any;
-
-  @Output() emitExam : any;
   public professor : ProfessorDto;
+  public creatingExam : boolean;
 
   constructor(
     private formBuilder : FormBuilder,
     private examService : ExamService,
-    private messageService : MessageService,
-    private professorService : ProfessorService
-    ) { 
-      this.selectedAccordition = false;
-      this.selectedAccorditionChange = new EventEmitter<boolean>();
+    private professorService : ProfessorService,
+    private messageService : MessageService
+    ) {
+      this.exams = new Array<ExamDto>();
+      this.examsChange = new EventEmitter<Array<ExamDto>>();
       this.creatingExam = true;
-      this.creatingExamChange = new EventEmitter<boolean>();
-      this.emitExam = new EventEmitter<ExamDto>();
+      this.exam = new ExamDto;
+      this.examForm = this.formBuilder.group({ 
+        name : new FormControl('', [Validators.required, Validators.maxLength(100)]),
+        durability : new FormControl(0, [Validators.required, Validators.min(1), Validators.max(7200)]),
+        maxGrade : new FormControl(0.0, [Validators.required, Validators.min(0.0), Validators.max(5.0)]),
+        description : new FormControl('', [Validators.required, Validators.min(1), Validators.max(1024)])}
+      );
       this.professor = new ProfessorDto;
-      this.form = this.formBuilder.group({
-        id : ['', []],
-        professor : new FormControl('', []),
-        name : new FormControl('', []),
-        durability : new FormControl('', []),
-        maxGrade : new FormControl('', []),
-        description : new FormControl('', []),
-        link : new FormControl('',[])
-      });
-      this.formChange = new EventEmitter<FormGroup>();
     }
+  
 
   ngOnInit(): void { 
-    //This code should no be here
-    this.professorService.getByUsername("professor").subscribe(
-      response => {this.professor = response;},
-      error => {console.log(error);}
+      //This code should no be here
+      this.setProfessor();
+  }
+
+  private setProfessor() : void {
+    this.professorService.getByUsername("professor").subscribe( 
+      response => {
+        this.professor = response;
+        this.setExams();
+      },
+      error => console.log(error)
     );
   }
 
-  private resetForm() : void {
-    this.form.reset();
-    this.formChange.emit(this.form);
+  private setExams() : void {
+    this.examService.getExamByProfessor(this.professor.id).subscribe(
+      response => {
+        this.exams = response;
+        this.examsChange.emit(this.exams);
+      },
+      error => console.log(error)
+    );
   }
 
-  private setExamMissingInfo() : ExamDto {
-    let exam : ExamDto = <ExamDto> this.form.value;
-    exam.professor = this.professor;
-    exam.link = 'localhost:4242/present-exam';
-    return exam;
+  private resetExamForm() : void {
+    this.examForm.reset({
+      name : '',
+      durability : 0,
+      maxGrade : 0.0,
+      description : ''
+    });
+  }
+
+  private generateExamLink() : string {
+    return 'localhost:4242/present-exam';
+  }
+
+  private extractExamInfoFromExamForm() : void {
+    let examForm : ExamDto = <ExamDto> this.examForm.value;
+    this.exam.name = examForm.name;
+    this.exam.durability = examForm.durability;
+    this.exam.maxGrade = examForm.maxGrade;
+    this.exam.description = examForm.description;
   }
 
   public saveExam() : void {
-    let exam : ExamDto =  this.setExamMissingInfo();
-    this.examService.saveExam(exam).subscribe(
+    this.exam.id = 0;
+    this.extractExamInfoFromExamForm();
+    this.exam.link = this.generateExamLink();
+    this.exam.professor = this.professor;
+
+    this.examService.saveExam(this.exam).subscribe(
       response => {
-        this.messageService.add({severity:'success', summary:'Success', detail:'Exam created successfully'});
-        this.emitExam.emit(response);
+        this.messageService.add({severity:'success', summary:'Success', detail:'Exam \"' + response.name + '\" created successfully'});
+        this.exams.push(response);
+        this.examsChange.emit(this.exams);
       },
       error => {
         console.log(error);
-        let errorStatus : string = error.status;
-        this.messageService.add({severity:'error', summary:errorStatus, 
-          detail:'Error', sticky : true});
-      
+        this.messageService.add({severity:'error', summary:error.status, detail:'Error', sticky : true});
     });
 
-    this.resetForm();
-    this.selectedAccorditionChange.emit(false);
+    this.resetExamForm();
   }
   
   public updateExam() : void {
-    let exam : ExamDto = <ExamDto> this.form.value;
-    this.examService.updateExam(exam).subscribe(
+    this.extractExamInfoFromExamForm();
+
+    this.examService.updateExam(this.exam).subscribe(
       response => {
-        this.messageService.add({severity:'success', summary:'Success', detail:'Exam updated successfully'});
-        this.emitExam.emit(response);
+        this.messageService.add({severity:'success', summary:'Success', detail:'Exam \"' + this.exam.name + '\" updated successfully'});
+        let index = this.exams.findIndex( (e) => e.id === response.id );
+        this.exams.splice(index, 1, response);
+        this.examsChange.emit(this.exams);
       },
       error => {
         console.log(error);
-        let errorStatus : string = error.status;
-        this.messageService.add({severity:'error', summary:errorStatus, detail:'Error', sticky : true});
+        this.messageService.add({severity:'error', summary:error.status, detail:'Error', sticky : true});
       }
     );
 
-    this.resetForm();
-    this.selectedAccorditionChange.emit(false);
-    this.creatingExamChange.emit(true);
+    this.resetExamForm();
+    this.creatingExam = true;
   }
 
   public cancelNewExam() : void {
-    this.selectedAccorditionChange.emit(false);
-    this.resetForm();
+    this.resetExamForm();
   }
 
   public cancelUpdateExam() : void {
-    this.creatingExamChange.emit(true);
-    this.selectedAccorditionChange.emit(false);
-    this.resetForm();
+    this.resetExamForm();
+    this.creatingExam = true;
   }
+
 }
