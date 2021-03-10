0986d440cc25adda86463dbcab0b4b9cbff50dc0
CJG <chjugomez@utp.edu.co>
2021-03-10 11:50:43 -0500

    answer option service

---
+++ src/app/services/answer-option.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { AnswerOptionDto } from '../dto/AnswerOptionDto';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AnswerOptionService {
+
+  constructor(private http : HttpClient) { }
+
+  public getAnsOpts() : Observable<Array<AnswerOptionDto>> {
+    return this.http.get<Array<AnswerOptionDto>>(environment.apiURL + 'ansopt/all');
+  }
+
+  public getAnsOptByQuestion(questionID : Number) : Observable<Array<AnswerOptionDto>> {
+    return this.http.get<Array<AnswerOptionDto>>(environment.apiURL + 'ansopt/' + questionID);
+  }
+
+  public saveQAnsOpt(ansOpt : AnswerOptionDto) : Observable<AnswerOptionDto> {
+    return this.http.post<AnswerOptionDto>(environment.apiURL + 'ansopt', ansOpt);
+  }
+
+  public updateAnsOpt(ansOpt : AnswerOptionDto) : Observable<AnswerOptionDto> {
+    return this.http.put<AnswerOptionDto>(environment.apiURL + 'ansopt', ansOpt);
+  }
+
+  public delAnsOpt(ansOpt : AnswerOptionDto) : Observable<AnswerOptionDto> {
+    return this.http.delete<AnswerOptionDto>(environment.apiURL + 'ansopt/' + ansOpt.id);
+  }
+}
