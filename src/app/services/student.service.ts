da5390963bf244045297bf77b64922100dffa656
CJG <chjugomez@utp.edu.co>
2021-03-26 19:03:50 -0500

    Implemented student service

---
+++ src/app/services/student.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { StudentDto } from '../dto/StudentDto';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StudentService {
+
+  constructor(private http : HttpClient) { }
+
+  public getStudents() : Observable<Array<StudentDto>> {
+    return this.http.get<Array<StudentDto>>(environment.apiURL + 'student/all');
+  }
+
+  public getStudentByIdentificationCard(identCard : string) : Observable<StudentDto> {
+    return this.http.get<StudentDto>(environment.apiURL + 'student/byIdentCard?id=' + identCard);
+  }
+
+  public saveStudent(student : StudentDto) : Observable<StudentDto> {
+    return this.http.post<StudentDto>(environment.apiURL + 'student', student);
+  }
+
+  public updateStudent(student : StudentDto) : Observable<StudentDto> {
+    return this.http.put<StudentDto>(environment.apiURL + 'student', student);
+  }
+
+  public delStudent(student : StudentDto) : Observable<StudentDto> {
+    return this.http.delete<StudentDto>(environment.apiURL + 'student/' + student.id);
+  }
+}
