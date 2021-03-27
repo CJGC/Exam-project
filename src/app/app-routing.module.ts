3378d7eaa1c8d159ab59233f4a516166e3e8e35a
CJG <chjugomez@utp.edu.co>
2021-03-26 19:06:00 -0500

    Router for present exam component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { CreateProfessorComponent } from './Componentes/create-professor/create-professor.component';
 import { ManageExamComponent } from './Componentes/manage-exam/manage-exam.component';
 import { PageNotFoundComponent } from './Componentes/page-not-found/page-not-found.component';
+import { PresentExamComponent } from './Componentes/present-exam/present-exam.component';
 import { ProfessorMainViewComponent } from './Componentes/professor-main-view/professor-main-view.component';
 
 const routes: Routes = [
@@ -12,6 +13,7 @@ const routes: Routes = [
       {path: 'manage-exam-view', component: ManageExamComponent}
     ]
   },
+  { path: 'present-exam/:id', component: PresentExamComponent},
   { path: '', redirectTo: 'professor-main-view', pathMatch: 'full'},
   { path: '**', component: PageNotFoundComponent}
 ];
