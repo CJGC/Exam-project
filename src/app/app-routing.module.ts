365ea0865ef1f5064d7df1a34b5adda6d27df331
CJG <chjugomez@utp.edu.co>
2021-03-31 18:29:28 -0500

    Added routing to exam student componentes for grades

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,18 @@ import { ManageExamComponent } from './Componentes/manage-exam/manage-exam.compo
 import { PageNotFoundComponent } from './Componentes/page-not-found/page-not-found.component';
 import { StudentFormComponent } from './Componentes/student-form/student-form.component';
 import { ProfessorMainViewComponent } from './Componentes/professor-main-view/professor-main-view.component';
+import { ExamGradeMainViewComponent } from './Componentes/exam-grade-main-view/exam-grade-main-view.component';
+import { ExamStudentsResponsesComponent } from './Componentes/exam-students-responses/exam-students-responses.component';
+import { ExamStudentResponseDetailComponent } from './Componentes/exam-student-response-detail/exam-student-response-detail.component';
 
 const routes: Routes = [
   { path: 'create-professor', component: CreateProfessorComponent},
   { path: 'professor-main-view', component: ProfessorMainViewComponent, 
     children: [
-      {path: 'manage-exam-view', component: ManageExamComponent}
+      {path: 'manage-exam-view', component: ManageExamComponent},
+      {path: 'exam-grade-main-view', component: ExamGradeMainViewComponent},
+      {path: 'exam-students-view', component:ExamStudentsResponsesComponent},
+      {path: 'exam-student-response-detail', component:ExamStudentResponseDetailComponent}
     ]
   },
   { path: 'present-exam/:id', component: StudentFormComponent},
