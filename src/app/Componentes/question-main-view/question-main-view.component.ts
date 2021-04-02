f2fdd86ffe3a00c19945a102a7ce67e362626570
CJG <chjugomez@utp.edu.co>
2021-04-02 11:44:31 -0500

    Implemented restriction for question weight

--- src/app/Componentes/question-main-view/question-main-view.component.ts
+++ src/app/Componentes/question-main-view/question-main-view.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MessageService } from 'primeng/api';
+import { QuestionDto } from 'src/app/dto/abstractDto/QuestionDto';
 import { OpenQuestionDto } from 'src/app/dto/OpenQuestionDto';
 import { QuestionService } from 'src/app/services/question.service';
 
@@ -24,6 +25,9 @@ export class QuestionMainViewComponent implements OnInit {
   @Input() creatingQuestion : boolean;
   @Output() creatingQuestionChange : any;
 
+  @Input() maxWeight : number;
+  @Output() maxWeightChange : any;
+
   
   constructor(
     private formBuilder : FormBuilder,
@@ -42,6 +46,9 @@ export class QuestionMainViewComponent implements OnInit {
 
     this.creatingQuestion = true;
     this.creatingQuestionChange = new EventEmitter<boolean>();
+
+    this.maxWeight = 0;
+    this.maxWeightChange = new EventEmitter<number>();
   }
 
   ngOnInit(): void {
@@ -54,12 +61,19 @@ export class QuestionMainViewComponent implements OnInit {
     });
   }
 
+  public addMaxWeight(question : QuestionDto) : void {
+    this.maxWeight += question.weight
+    this.maxWeight = Number(this.maxWeight.toPrecision(2));
+    this.maxWeightChange.emit(this.maxWeight);
+  }
+
   public updateQuestion(question : OpenQuestionDto) : void {
     this.question = question;
     this.questionChange.emit(this.question);
     this.creatingQuestionChange.emit(false);
     this.putQuestionInfoIntoQuestionForm(question);
     this.questionFormChange.emit(this.questionForm);
+    this.addMaxWeight(question);
   }
 
   public delQuestion(question : OpenQuestionDto) : void {
@@ -68,6 +82,7 @@ export class QuestionMainViewComponent implements OnInit {
         this.messageService.add({severity:'success', summary:'Success', detail:'Question deleted successfully'});
         this.questions.splice(this.questions.indexOf(question), 1);
         this.questionsChange.emit(this.questions);
+        this.addMaxWeight(question);
       },
       error => console.log(error)
     );
