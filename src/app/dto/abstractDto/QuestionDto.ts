1b7cf4e56922dfa1c283b9cd7167c9546c6de5b4
CJG <chjugomez@utp.edu.co>
2021-04-04 17:39:04 -0500

    question image datatype changed

--- src/app/dto/abstractDto/QuestionDto.ts
+++ src/app/dto/abstractDto/QuestionDto.ts
@@ -4,7 +4,7 @@ export abstract class QuestionDto {
 
     public id : number;
     public description : string;
-    public questionImage : File;
+    public questionImage : string;
     public weight : number;
     public exam : ExamDto;
     public type : string;
@@ -12,7 +12,7 @@ export abstract class QuestionDto {
     constructor() {
         this.id = 0;
         this.description = "";
-        this.questionImage = new File([], "");
+        this.questionImage = "";
         this.weight = 0.0;
 
         // op -> open | mu -> multiple unique | mm -> multiple multiple
