e319d5fb3d1251a346cea3939bd58524e76ea2e8
CJG <chjugomez@utp.edu.co>
2021-06-21 14:07:17 -0500

    Added copyLink method and form to display data

--- src/app/Componentes/exam-details-view/exam-details-view.component.ts
+++ src/app/Componentes/exam-details-view/exam-details-view.component.ts
@@ -7,33 +7,46 @@ import { AnswerOptionService } from 'src/app/services/answer-option.service';
 import { QuestionService } from 'src/app/services/question.service';
 import { environment } from 'src/environments/environment';
 import { AnsOptsDetailsComponent } from '../ans-opts-details/ans-opts-details.component';
+import { Clipboard } from '@angular/cdk/clipboard';
+import { MessageService } from 'primeng/api';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-exam-details-view',
   templateUrl: './exam-details-view.component.html',
   styleUrls: ['./exam-details-view.component.css'],
-  providers: [DialogService]
+  providers: [DialogService, MessageService]
 })
 export class ExamDetailsViewComponent implements OnInit {
 
   public exam : ExamDto;
   public questions : Array<QuestionDto>;
   public ansOpts  : Array<AnswerOptionDto>;
+  public examDetails: FormGroup;
 
   constructor(
     private questionService : QuestionService,
     private ansOptService : AnswerOptionService,
     private config : DynamicDialogConfig,
-    private dialogService : DialogService
+    private dialogService : DialogService,
+    private clipboard : Clipboard,
+    private messageService : MessageService,
+    private formBuilder : FormBuilder
     ) { 
-
-      this.exam = new ExamDto
+      this.exam = this.config.data;
       this.questions = new Array<QuestionDto>();
       this.ansOpts = new Array<AnswerOptionDto>();
+      let grade =  (<number> this.exam.maxGrade * 100).toPrecision(2) + '%';
+      this.examDetails = this.formBuilder.group({
+        name : new FormControl({value : this.exam.name, disabled: true}),
+        link : new FormControl({value : this.exam.link, disabled: true}),
+        maxGrade : new FormControl({value : grade, disabled: true}),
+        durability : new FormControl({value: this.exam.durability, disabled: true}),
+        description : new FormControl({value: this.exam.description, disabled: true})
+      });
     }
 
   ngOnInit(): void {
-    this.exam = this.config.data;
     this.getQuestions();
   }
 
@@ -70,5 +83,13 @@ export class ExamDetailsViewComponent implements OnInit {
       },
       error => console.log(error)
     );
+  }
+
+  public copyLink() : void {
+    this.clipboard.copy(this.exam.link.toString());
+    this.messageService.add({
+      severity:'success', 
+      summary: 'Copied successfully', 
+      detail:'Link was copied to clipboard sucessfully'});
   }
 }
