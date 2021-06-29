2bee45e39dc9ea507af2d0bd6167aeabb55b82c0
CJG <chjugomez@utp.edu.co>
2021-06-29 16:41:39 -0500

    Redirect to query student grade component added, removed unused flags, imgUrl bug was fiex

--- src/app/Componentes/solve-exam/solve-exam.component.ts
+++ src/app/Componentes/solve-exam/solve-exam.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CountdownComponent } from 'ngx-countdown';
 import { MessageService } from 'primeng/api';
 import { QuestionDto } from 'src/app/dto/abstractDto/QuestionDto';
 import { AnswerOptionDto } from 'src/app/dto/AnswerOptionDto';
@@ -14,6 +16,7 @@ import { ExamStudentService } from 'src/app/services/exam-student.service';
 import { OpenResponseService } from 'src/app/services/open-response.service';
 import { QuestionService } from 'src/app/services/question.service';
 import { SelectedResponseService } from 'src/app/services/selected-response.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-solve-exam',
@@ -23,20 +26,22 @@ import { SelectedResponseService } from 'src/app/services/selected-response.serv
 })
 export class SolveExamComponent implements OnInit {
 
-  @Input() public exam : ExamDto;
-  @Input() public student : StudentDto;
-  @Input() public examStudent : ExamStudentDto;
+  @ViewChild('cd', { static: false })
+  private countdown!: CountdownComponent;
+  public exam : ExamDto;
+  public student : StudentDto;
+  public examStudent : ExamStudentDto;
+  public examForm : FormGroup;
 
   public questions : Array<QuestionDto>;
-  public question : QuestionDto;
+  public currentQuestion : QuestionDto;
   public questionIndex : number;
   public openResponseForm : FormGroup;
   public ansOpts : Array<any>;
   public loadedAnsOpts : Array<AnswerOptionDto>;
   public maxGrade : number;
   public imgUrl : any;
-  public thereWereOpenQuestion : boolean;
-  public examHasBeenEnded : boolean;
+  public examHasStarted : boolean;
 
   public responses : Array<Array<any>>;
 
@@ -50,19 +55,26 @@ export class SolveExamComponent implements OnInit {
     private openResponseService : OpenResponseService,
     private selectedResponseService : SelectedResponseService,
     private formBuilder : FormBuilder,
-    public messageService : MessageService
-  ) { 
+    public messageService : MessageService,
+    private router : Router
+  ) {
     this.exam = new ExamDto;
     this.student = new StudentDto;
-    this.examStudent = new ExamStudentDto
+    this.examStudent = new ExamStudentDto;
     this.questions = new Array<QuestionDto>();
-    this.question = new OpenQuestionDto;
-    this.question.type = "";
+    this.examHasStarted = false;
+    this.currentQuestion = new OpenQuestionDto;
+    this.currentQuestion.type = "";
     this.questionIndex = 0;
     this.ansOpts = [];
     this.loadedAnsOpts = new Array<AnswerOptionDto>();
     this.maxGrade = 5.0;
-
+    this.examForm = this.formBuilder.group({
+      name : new FormControl({value :'', disabled : true}),
+      durability : new FormControl({value :'', disabled : true}),
+      maxGrade: new FormControl({value : '', disabled : true}),
+      description: new FormControl({value :'', disabled : true})
+    });
     this.responses = [];
     this.openResponseForm = this.formBuilder.group({
       content : new FormControl('', [Validators.max(2048)])
@@ -71,11 +83,44 @@ export class SolveExamComponent implements OnInit {
     this.selectedAnsOpt = new AnswerOptionDto;
     this.selectedAnsOpts = new Array<AnswerOptionDto>();
     this.imgUrl = "";
-    this.thereWereOpenQuestion = false;
-    this.examHasBeenEnded = false;
   }
 
   ngOnInit(): void {
+    this.setExam();
+    this.setExamForm();
+    this.setStudent();
+    this.setQuestionsByExam();
+  }
+
+  private setExamForm() : void {
+    let examDurability = this.exam.durability;
+    let hours = Math.floor(examDurability / 3600);
+    examDurability -= hours * 3600;
+    let minutes = Math.floor(examDurability / 60);
+    examDurability -= minutes * 60;
+    let seconds = examDurability;
+    let durabilityNewFormat = hours + ':' + minutes + ':' + seconds;
+    let examGradeNewFormat = (this.exam.maxGrade * 100).toPrecision(2) + '%';
+    
+    this.examForm.setValue({
+      name : this.exam.name,
+      durability : durabilityNewFormat,
+      maxGrade: examGradeNewFormat,
+      description: this.exam.description
+    });
+  }
+
+  private setExam() : void {
+    let examStringify = sessionStorage.getItem('exam');
+    this.exam = (examStringify) ? JSON.parse(examStringify) : undefined;
+  }
+
+  private setStudent() : void {
+    let studentStringify = sessionStorage.getItem('student');
+    this.student = (studentStringify) ? JSON.parse(studentStringify) : undefined;
+  }
+
+  private setQuestionsByExam() : void {
     this.questionService.getQuestionByExam(this.exam.id).subscribe(
       questions => {
         this.questions = questions;
@@ -86,65 +131,63 @@ export class SolveExamComponent implements OnInit {
     );
   }
 
-  public finishExamByTimeOut(event : any) : void {
-    if (event.action === 'done') {
-      this.saveExamStudent();
-    }
-  }
-
-  private getImage(question : QuestionDto) : void {
-
-    if (question.questionImage === "") {
-      return;
-    }
-    
-    this.questionService.getImage(question.questionImage).subscribe(
-      image => {
-        let reader = new FileReader();
-        reader.readAsDataURL(image);
-        reader.onload = () => {
-          this.imgUrl = reader.result;
-        }
-      },
-      error => console.error(error)
-    );
-  }
-
-  private resetImage() : void {
-    this.imgUrl = "";
-  }
-
   private setFirstQuestion() : void {
     let FIRST_QUESTION = 0;
     this.questionIndex = FIRST_QUESTION;
-    this.question = this.questions[FIRST_QUESTION];
+    this.currentQuestion = this.questions[FIRST_QUESTION];
     
-    if (this.question.questionImage !== "") {
-      this.getImage(this.question);
+    if (this.currentQuestion.questionImage !== "") {
+      this.putImage(this.currentQuestion);
     }
     // if question type is not open question, set answer options
-    (this.question.type!=='op') ? this.getAnsOpts(this.question) : false;
+    (this.currentQuestion.type!=='op') ? this.getAnsOpts(this.currentQuestion) : false;
   }
 
   private setResponses() : void {
     this.questions.forEach( question => this.responses.push([]));
   }
 
+  public startExam() : void {
+    this.examHasStarted = true;
+  }
+
+  public finishExamByTimeOut(event : any) : void {
+    if (event.action === 'done') {
+      this.countdown?.stop();
+      this.saveExamStudent();
+    }
+  }
+
+  private putImage(question : QuestionDto) : void {
+    this.imgUrl = (question.questionImage !== "") ? `${environment.apiURL}question/getImage/?filename=${question.questionImage}` : "";
+  }
+
   private getAnsOpts(question : QuestionDto) : void {
     this.ansOptService.getAnsOptByQuestion(question.id).subscribe(
       ansOpts => {
-        this.ansOpts.push({question : question, ansOpts : ansOpts})
+        this.ansOpts.push({question : question, ansOpts : ansOpts});
         this.loadAnsOpts();
       },
       error => console.log(error)
     );
   }
 
+  private loadAnsOpts() : boolean {
+    for (let i=0; i<this.ansOpts.length; i++) {
+      let pair = this.ansOpts[i];
+      if (pair.question === this.currentQuestion) {
+          this.loadedAnsOpts = pair.ansOpts;
+          return true;
+      }
+    }
+    return false;
+  }
+
   private setOpenResponseInfo(openResponse : OpenResponseDto) {
     let openResponseForm = this.openResponseForm.value;
     openResponse.content = openResponseForm.content;
     openResponse.examStudent = this.examStudent;
-    openResponse.question = this.question;
+    openResponse.question = this.currentQuestion;
     openResponse.valoration = 0.0;
   }
 
@@ -213,29 +256,19 @@ export class SolveExamComponent implements OnInit {
     this.selectedAnsOpts = this.responses[this.questionIndex];
   }
 
-  private loadAnsOpts() : boolean {
-    for (let i=0; i<this.ansOpts.length; i++) {
-      let pair = this.ansOpts[i];
-      if (pair.question === this.question) {
-          this.loadedAnsOpts = pair.ansOpts;
-          return true;
-      }
-    }
-    return false;
-  }
 
   private checkAndLoadAnsOpts() : void {
     let existAnsOpts : boolean = false;
     existAnsOpts = this.loadAnsOpts();
-    (!existAnsOpts) ? this.getAnsOpts(this.question) : false;
+    (!existAnsOpts) ? this.getAnsOpts(this.currentQuestion) : false;
   }
 
   private addStudentResponse() : void {
-    if (this.question.type==="op") {
+    if (this.currentQuestion.type==="op") {
       this.addOpenResponse();
       this.clearOpenResponseForm();
     }
-    else if (this.question.type==="mu") {
+    else if (this.currentQuestion.type==="mu") {
       this.addMultiUniqueResponse();
     } // mm question type
     else {
@@ -249,14 +282,13 @@ export class SolveExamComponent implements OnInit {
 
     /* loading student answer of next/back question if it exist */
     this.questionIndex += questionIndexOperation;
-    this.question = this.questions[this.questionIndex];
-    this.resetImage();
-    this.getImage(this.question);
+    this.currentQuestion = this.questions[this.questionIndex];
+    this.putImage(this.currentQuestion);
 
-    if (this.question.type==="op") {
+    if (this.currentQuestion.type==="op") {
       this.loadOpenResponse();
     }
-    else if (this.question.type==="mu") {
+    else if (this.currentQuestion.type==="mu") {
       this.checkAndLoadAnsOpts();
       this.loadMultiUniqueResponse();
     } // mm question type
@@ -296,6 +328,8 @@ export class SolveExamComponent implements OnInit {
   }
 
   public saveExamStudent() : void {
+    this.examStudent.exam = this.exam;
+    this.examStudent.student = this.student;
     this.examStudentService.saveExamStudent(this.examStudent).subscribe(
       examStudent => {
         this.examStudent = examStudent;
@@ -329,7 +363,6 @@ export class SolveExamComponent implements OnInit {
         if (openResponse) {
           openResponse.examStudent = this.examStudent;
           this.saveOpenResponse(openResponse);
-          this.thereWereOpenQuestion = true;
         }
       } 
       
@@ -363,7 +396,6 @@ export class SolveExamComponent implements OnInit {
 
     this.examStudent.definitiveGrade = studentGrade;
     this.updateExamStudent();
-    this.questions = new Array<QuestionDto>();
-    this.examHasBeenEnded = true;
+    this.router.navigate(['./query-grade']);
   }
 }
